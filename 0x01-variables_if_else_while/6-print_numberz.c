08c26df408a9440dfa00477e1cd9348edbab10a6
eliasonyango <eliasonyango15@gmail.com>
2023-06-16 20:54:12 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 57)
+	{
+		putchar(digit);
+		digit++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
