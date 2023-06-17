f7b21bf4fe60eb3109ff3ea4fbfb88febe6f66f4
eliasonyango <eliasonyango15@gmail.com>
2023-06-16 20:45:23 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
