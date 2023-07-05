3ab2b44c552b78f6f958a31208c37ca8f8f1edef
eliasonyango <eliasonyango15@gmail.com>
2023-07-05 05:18:18 -0700

    You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - Computes the factorial of a number.
+ * @n: The number to compute the factorial of.
+ *
+ * Return: The factorial of the number, or -1 if an error occurs.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
